a235f972f0ebaee7c7e62caa883f9f3656ca4003
AlexanderDenisov <ad060778@gmail.com>
2015-02-22 01:01:08 +0300

    Создан класс работающий с БД

--- www/index.php
+++ www/index.php
@@ -1,3 +1,23 @@
 <?php
 
-phpinfo();
+class Database {
+    private $db = null;
+    private $result = null;
+    public function __construct($host, $user, $password, $base)
+    {
+        $this->db = new mysqli($host, $user, $password, $base);
+        //$this->db = mysql_select_db($base);
+    }
+    public function query($query)
+    {
+        $this->result = $this->db->query($query);
+         while(false !== ($row = $this->result->fetch_array())) {
+             $data[] = $row;
+             return $data;
+         }
+    }
+}
+
+$db = new Database('localhost', 'root', '', 'test');
+$data = $db->query('SELECT * FROM news');
+var_dump($data);

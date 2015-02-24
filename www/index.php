f775bd2c75bca7fd3755d9d61554f5fef6aaf751
AlexanderDenisov <ad060778@gmail.com>
2015-02-25 00:05:45 +0300

    Изменен

--- www/index.php
+++ www/index.php
@@ -1,67 +1,8 @@
 <?php
 
-class Database
-    {
+require_once __DIR__ . '/models/News.php';
 
-    public function __construct($host, $user, $password, $base)
-    {
-        mysql_connect($host, $user, $password);
-        mysql_select_db($base);
-    }
-    public function query($sql)
-    {
-        $result = mysql_query($sql);
-        $data = [];
-         while(false !== ($row = mysql_fetch_assoc($result))) {
-             $data[] = $row;
-         } return $data;
-    }
-    public function exec($sql)
-    {
-        $result = mysql_query($sql);
-        return $result;
-    }
-    public function select()
-    {
-        $sql = 'SELECT * FROM news';
-        return $this->query($sql);
-    }
-    public function insert()
-    {
-        $sql = 'INSERT INTO news (id, date, title, path) VALUES (NULL, \'22.02.2015\', \'Привет лунатикам\', \'\')';
-        return $this->exec($sql);
-    }
-    public function delete()
-    {
-        $sql = 'DELETE FROM news WHERE title=\'Привет лунатикам\'';
-        return $this->exec($sql);
-    }
-}
+$items = News::getAll();
 
-$db = new Database('localhost', 'root', '', 'test');
-$data = $db->query('SELECT * FROM news');
-var_dump($data);
 
-echo '<BR>';
-echo '<BR>';
-echo '<BR>';
-
-$db2 = new Database('localhost', 'root', '', 'test');
-$data2 = $db2->select();
-var_dump($data2);
-
-echo '<BR>';
-echo '<BR>';
-echo '<BR>';
-
-$db3 = new Database('localhost', 'root', '', 'test');
-$data3 = $db3->insert();
-var_dump($data3);
-
-echo '<BR>';
-echo '<BR>';
-echo '<BR>';
-
-$db4 = new Database('localhost', 'root', '', 'test');
-$data4 = $db4->delete();
-var_dump($data4);
+include __DIR__ . '/views/index.php';
---
+++ www/views/index.php
@@ -0,0 +1,4 @@
+<?php foreach ($items as $item): ?>
+    <h1><?php echo $item->title; ?></h1>
+    <div><?php echo $item->date; echo $item->path; ?></div>
+    <?php endforeach; ?>

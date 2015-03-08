33ded1fe4c56949fb7a5a6c0154495ea0bd37170
AlexanderDenisov <ad060778@gmail.com>
2015-03-08 19:25:17 +0300

    Исправлена функция FindByColumn

--- www/controllers/Controller_FindByColumn.php
+++ www/controllers/Controller_FindByColumn.php
@@ -7,7 +7,6 @@ class Controller_FindByColumn
         $column = $_POST['column'];
         $values = $_POST['values'];
         $item = NewsModel::findByColumn($column, $values);
-        //var_dump($items);
         $view = new View();
         $view->item = $item;
         $view->display('template_OneNews.php');

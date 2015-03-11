0dae38024b606bd0688d7b0878d3a91610a8bd7d
AlexanderDenisov <ad060778@gmail.com>
2015-03-12 01:31:35 +0300

    установлен composer. Попытка работы с PHPMailer

--- www/autoload.php
+++ www/autoload.php
@@ -1,21 +1,15 @@
 <?php
 
-function __autoload($class)
+function autoloader($class)
 {
-
-    if (file_exists(__DIR__ . '/controllers/' . $class . '.php')) {
-        require __DIR__ . '/controllers/' . $class . '.php';
-    } elseif (file_exists(__DIR__ . '/models/' . $class . '.php')) {
-        require __DIR__ . '/models/' . $class . '.php';
-    } elseif (file_exists(__DIR__ . '/classes/' . $class . '.php')) {
-        require __DIR__ . '/classes/' . $class . '.php';
-    } else {
         $classParts = explode('\\', $class);
         $classParts[0] = __DIR__;
         $path = implode(DIRECTORY_SEPARATOR, $classParts) . '.php';
         if(file_exists($path)) {
             require $path;
         }
-    }
-
 }
+
+spl_autoload_register('autoloader');
+require __DIR__ .'/../vendor/autoload.php';
+//require __DIR__ . '/../vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
--- www/controllers/AddNews.php
+++ www/controllers/AddNews.php
@@ -3,6 +3,8 @@
 namespace Application\Controllers;
 use Application\Models\News;
 use Application\Classes\View;
+use Application\Classes\E404Exception;
+use Application\Classes\My_phpmailer;
 
 class AddNews
 {
@@ -14,6 +16,20 @@ class AddNews
         $article->title = $_POST['title'];
         $article->text_news = $_POST['text_news'];
         $article->save();
+
+        $mail = new My_phpmailer;
+
+        $mail->addAddress('ad060778@gmail.com', 'Александр Денисов');
+        $mail->Subject = 'Добавлена новая статья';
+        $mail->Body = 'Новая статья';
+
+        if(!$mail->send()) {
+            $e = new E404Exception(date('d.m.Y H:i:s') . ' Сообщение о добавлении новой статьи не отправлено!!!' . "\r\n");
+            throw $e;
+        }
+        $mail->clearAddresses();
+        $mail->clearAttachments();
+
         $view = new View();
         $view->item = $article;
         $view->display('template_OneNews.php');
